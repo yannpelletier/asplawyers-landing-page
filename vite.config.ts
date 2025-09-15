296da45b0875b248f4750993ffdb67598a1b5238
Yann Pelletier <33026902+yannpelletier@users.noreply.github.com>
2025-09-15 18:11:14 -0400

    Test

--- vite.config.ts
+++ vite.config.ts
@@ -8,51 +8,51 @@ export default defineConfig({
 		paraglideVitePlugin({
 			project: './project.inlang',
 			outdir: './src/lib/paraglide',
-			strategy: ['url', 'baseLocale'],
-			urlPatterns: [
-				{
-					pattern: '/',
-					localized: [
-						['en', '/en'],
-						['fr', '/']
-					]
-				},
-				{
-					pattern: '/contact',
-					localized: [
-						['en', '/en/contact'],
-						['fr', '/contact']
-					]
-				},
-				{
-					pattern: '/about',
-					localized: [
-						['en', '/en/about'],
-						['fr', '/a-propos']
-					]
-				},
-				{
-					pattern: '/services',
-					localized: [
-						['en', '/en/services'],
-						['fr', '/services']
-					]
-				},
-				{
-					pattern: '/privacy-policy',
-					localized: [
-						['en', '/en/privacy-policy'],
-						['fr', '/politique-de-confidentialite']
-					]
-				},
-				{
-					pattern: '/careers',
-					localized: [
-						['en', '/en/careers'],
-						['fr', '/carrieres']
-					]
-				}
-			]
+			strategy: ['url', 'baseLocale']
+			// urlPatterns: [
+			// 	{
+			// 		pattern: '/',
+			// 		localized: [
+			// 			['en', '/en'],
+			// 			['fr', '/']
+			// 		]
+			// 	},
+			// 	{
+			// 		pattern: '/contact',
+			// 		localized: [
+			// 			['en', '/en/contact'],
+			// 			['fr', '/contact']
+			// 		]
+			// 	},
+			// 	{
+			// 		pattern: '/about',
+			// 		localized: [
+			// 			['en', '/en/about'],
+			// 			['fr', '/a-propos']
+			// 		]
+			// 	},
+			// 	{
+			// 		pattern: '/services',
+			// 		localized: [
+			// 			['en', '/en/services'],
+			// 			['fr', '/services']
+			// 		]
+			// 	},
+			// 	{
+			// 		pattern: '/privacy-policy',
+			// 		localized: [
+			// 			['en', '/en/privacy-policy'],
+			// 			['fr', '/politique-de-confidentialite']
+			// 		]
+			// 	},
+			// 	{
+			// 		pattern: '/careers',
+			// 		localized: [
+			// 			['en', '/en/careers'],
+			// 			['fr', '/carrieres']
+			// 		]
+			// 	}
+			// ]
 		}),
 		tailwindcss(),
 		sveltekit()
