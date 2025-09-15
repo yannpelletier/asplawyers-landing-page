ad4d8f263fa272219583e4e9b4108104c8c82d0d
Yann Pelletier <33026902+yannpelletier@users.noreply.github.com>
2025-09-15 18:02:15 -0400

    Fixed crawling

--- src/routes/+layout.svelte
+++ src/routes/+layout.svelte
@@ -1,8 +1,11 @@
 <script lang="ts">
 	import { ModeWatcher } from 'mode-watcher';
+	import { page } from '$app/state';
+	import { locales, localizeHref } from '$lib/paraglide/runtime';
 	import Header from '$lib/components/header.svelte';
 	import Footer from '$lib/components/footer.svelte';
 	import CookieBar from '$lib/components/cookie-bar.svelte';
+
 	import '../app.css';
 </script>
 
@@ -10,6 +13,11 @@
 <div class="flex min-h-screen flex-col">
 	<Header />
 	<main>
+		<div style="display: none">
+			{#each locales as locale}
+				<a href={localizeHref(page.url.pathname, { locale })} aria-label={locale}></a>
+			{/each}
+		</div>
 		<slot />
 	</main>
 	<Footer />
